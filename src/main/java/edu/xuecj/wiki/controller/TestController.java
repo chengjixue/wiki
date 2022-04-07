e1a38e3e8188280fb661fe4a099da366a7554bc8
xuecj <xuecjpersonal@163.com>
2022-04-07 23:28:19 +0800

    增加配置项的示例

--- src/main/java/edu/xuecj/wiki/controller/TestController.java
+++ src/main/java/edu/xuecj/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package edu.xuecj.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 //RestController是用于返回字符串
 
 public class TestController {
+//    在  ：后面加入值在没有配置情况下使用
+    @Value("${test.hello:TEST}")
+    private String testHello;
     @RequestMapping("/hello")
     public String hello(){
-        return "hello";
+        return "hello"+testHello;
     }
 }
