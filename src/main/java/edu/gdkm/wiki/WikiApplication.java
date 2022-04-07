2aed180e738d261374d268809bfa82c3129a87d7
xuecj <xuecjpersonal@163.com>
2022-04-07 21:46:36 +0800

    增加logback配置文件

--- src/main/java/edu/gdkm/wiki/WikiApplication.java
+++ src/main/java/edu/gdkm/wiki/WikiApplication.java
@@ -2,12 +2,14 @@ package edu.gdkm.wiki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WikiApplication.class, args);
+
     }
 
 }
