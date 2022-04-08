2ffd0da3a477702588f967d6eebd3e83ad2e40d4
xuecj <xuecjpersonal@163.com>
2022-04-08 11:16:35 +0800

    增加all.sql文件

---
+++ doc/all.sql
@@ -0,0 +1,7 @@
+drop table if exists 'test';
+create table `test` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) not null comment '名称',
+                        `password` int comment '密码',
+                        primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='测试';
