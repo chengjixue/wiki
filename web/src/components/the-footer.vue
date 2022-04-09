d91af27b8125dd5755d4d54bdbc917e91c45fa3d
xuecj <xuecjpersonal@163.com>
2022-04-09 16:57:58 +0800

    制作the-footer组件,去掉helloworld相关代码

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Java电子书
+  </a-layout-footer>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+});
+</script>
