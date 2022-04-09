4bb42093183be4487205611f8b4c6334df64b81a
xuecj <xuecjpersonal@163.com>
2022-04-09 10:39:09 +0800

    封装请求参数和返回参数

--- src/main/java/edu/xuecj/wiki/controller/EbookController.java
+++ src/main/java/edu/xuecj/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package edu.xuecj.wiki.controller;
 
-import edu.xuecj.wiki.domain.Ebook;
+import edu.xuecj.wiki.req.EbookReq;
 import edu.xuecj.wiki.resp.CommonResp;
+import edu.xuecj.wiki.resp.EbookResp;
 import edu.xuecj.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +23,9 @@ public class EbookController {
     @Resource
     private EbookService ebookService;
     @GetMapping("/list")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list=ebookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list=ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/edu/xuecj/wiki/service/EbookService.java
+++ src/main/java/edu/xuecj/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package edu.xuecj.wiki.service;
 
 import edu.xuecj.wiki.domain.Ebook;
+import edu.xuecj.wiki.domain.EbookExample;
 import edu.xuecj.wiki.mapper.EbookMapper;
+import edu.xuecj.wiki.req.EbookReq;
+import edu.xuecj.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,8 +22,23 @@ public class EbookService {
 //    @Resource是jdk自带的注解 也可以用 @Autowired
     @Resource
     private EbookMapper ebookMapper;
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq  req){
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        /*
+        * criteria.andNameLike("%"+name+"%");
+        * 相当于like的效果，用来模糊查询
+        * */
+        criteria.andNameLike("%"+req.getName()+"%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        List<EbookResp> respList=new ArrayList<>();
+        for (Ebook ebook:ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            ebookResp.setId(ebook.getId());
+            BeanUtils.copyProperties(ebook,ebookResp);
+            respList.add(ebookResp);
+        }
+        return respList;
     }
 
 }
