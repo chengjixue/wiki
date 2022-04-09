471aa3455ac2ed3df0bf1cbbc7172a07bc5ece75
xuecj <xuecjpersonal@163.com>
2022-04-09 11:02:28 +0800

    制作CopUtil封装BeanUtils

--- src/main/java/edu/xuecj/wiki/service/EbookService.java
+++ src/main/java/edu/xuecj/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import edu.xuecj.wiki.domain.EbookExample;
 import edu.xuecj.wiki.mapper.EbookMapper;
 import edu.xuecj.wiki.req.EbookReq;
 import edu.xuecj.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import edu.xuecj.wiki.utils.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,26 +18,34 @@ import java.util.List;
  */
 @Service
 public class EbookService {
-//    @Resource是jdk自带的注解 也可以用 @Autowired
+    //    @Resource是jdk自带的注解 也可以用 @Autowired
     @Resource
     private EbookMapper ebookMapper;
-    public List<EbookResp> list(EbookReq  req){
+
+    public List<EbookResp> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         /*
-        * criteria.andNameLike("%"+name+"%");
-        * 相当于like的效果，用来模糊查询
-        * */
-        criteria.andNameLike("%"+req.getName()+"%");
+         * criteria.andNameLike("%"+name+"%");
+         * 相当于like的效果，用来模糊查询
+         * */
+        criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList=new ArrayList<>();
-        for (Ebook ebook:ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            ebookResp.setId(ebook.getId());
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+
+//        List<EbookResp> respList=new ArrayList<>();
+//        for (Ebook ebook:ebookList) {
+
+//        spring自带的对象复制
+//        EbookResp ebookResp = new EbookResp();
+//        BeanUtils.copyProperties(ebook,ebookResp);
+
+//        utils工具类对象复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+//        列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 
 }
