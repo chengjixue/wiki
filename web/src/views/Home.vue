3b48b73d130bb07229c9539da5d4a7f6dc46c5b1
xuecj <xuecjpersonal@163.com>
2022-04-10 13:42:20 +0800

    电子书布局调整 行显示二列电子书

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -45,35 +45,23 @@
     </a-layout-sider>
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
-      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-        <template #footer>
-          <div>
-            <b>ant design vue</b>
-            footer part
-          </div>
-        </template>
+      <a-list item-layout="vertical" size="large" :grid="{gutter:20,columns:3}" :data-source="ebooks">
         <template #renderItem="{ item }">
-          <a-list-item key="item.title">
+          <a-list-item key="item.name">
             <template #actions>
           <span v-for="{ type, text } in actions" :key="type">
-            <component v-bind:is="type" style="margin-right: 8px" />
+            <component v-bind:is="type" style="margin-right: 8px"/>
             {{ text }}
           </span>
             </template>
-            <template #extra>
-              <img
-                  width="272"
-                  alt="logo"
-                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-              />
-            </template>
             <a-list-item-meta :description="item.description">
               <template #title>
-                <a :href="item.href">{{ item.title }}</a>
+                <a :href="item.href">{{ item.name }}</a>
               </template>
-              <template #avatar><a-avatar :src="item.avatar" /></template>
+              <template #avatar>
+                <a-avatar :src="item.cover"/>
+              </template>
             </a-list-item-meta>
-            {{ item.content }}
           </a-list-item>
         </template>
       </a-list>
@@ -82,7 +70,7 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, ref,reactive,toRef} from 'vue';
+import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios';
 
 const listData: any = [];
@@ -119,17 +107,17 @@ export default defineComponent({
       ebooks,
       ebooks2: toRef(ebooks1, "books"),
       listData,
-      pagination :{
+      pagination: {
         onChange: (page: any) => {
           console.log(page);
         }, pagesize: 3,
       },
-           actions:  [
-            {type: 'StarOutlined', text: '156'},
-            {type: 'LikeOutlined', text: '156'},
-            {type: 'MessageOutlined', text: '2'},],
-        }
-      }
+      actions: [
+        {type: 'StarOutlined', text: '156'},
+        {type: 'LikeOutlined', text: '156'},
+        {type: 'MessageOutlined', text: '2'},],
+    }
+  }
 
-  });
+});
 </script>
