ec8d6a398f8762a3279a43cf22d050eef7bed429
xuecj <xuecjpersonal@163.com>
2022-04-10 18:13:03 +0800

    增加拦截器，打印接口耗时

---
+++ src/main/java/edu/xuecj/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,24 @@
+package edu.xuecj.wiki.config;
+
+import edu.xuecj.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * @author xuecj
+ * @version 1.0
+ * @date 2022/4/10 18:05
+ */
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Resource
+    LogInterceptor logInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login");
+    }
+}
