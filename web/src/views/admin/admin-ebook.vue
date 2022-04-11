c3aa7e8d34d45b5c3d3a46f6d49ed4dfbd0e686d
xuecj <xuecjpersonal@163.com>
2022-04-11 17:02:21 +0800

    点击编辑按钮弹出模态框

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -15,7 +15,7 @@
         </template>
         <template v-slot:action="{ text, record }">
           <a-space size="small">
-            <a-button type="primary">
+            <a-button type="primary" @click="edit">
               编辑
             </a-button>
             <a-button type="danger">
@@ -26,6 +26,14 @@
       </a-table>
     </a-layout-content>
   </a-layout>
+  <a-modal
+      title="电子书表单"
+      v-model:visible="modalVisible"
+      :confirm-loading="modalLoading"
+      @ok="handleModalOk"
+  >
+    <p>test</p>
+  </a-modal>
 </template>
 
 <script lang="ts">
@@ -111,6 +119,22 @@ export default defineComponent({
       });
     };
 
+    //表单
+    const modalVisible=ref(false);
+    const modalLoading=ref(false);
+    const handleModalOK=()=>{
+      modalLoading.value=true;
+      setTimeout(()=>{
+        modalVisible.value=false;
+        modalLoading.value=false;
+      },2000)
+    }
+    /*
+    * 编辑*/
+    const edit = () => {
+      modalVisible.value=true;
+    };
+
     onMounted(() => {
       handleQuery({
         page: 1,
@@ -123,7 +147,11 @@ export default defineComponent({
       pagination,
       columns,
       loading,
-      handleTableChange
+      handleTableChange,
+      edit,
+      modalVisible,
+      modalLoading,
+      handleModalOK
     }
   }
 });
