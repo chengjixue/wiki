1545e5658da614f05c0a7a38ea83c1bc624330f1
xuecj <xuecjpersonal@163.com>
2022-04-17 21:00:46 +0800

    8-9 让表格树默认展开

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -12,12 +12,14 @@
           </a-form>
         </p>
         <a-table
+            v-if="level1.length>0"
             :columns="columns"
             :row-key="record => record.id"
             :data-source="level1"
             :loading="loading"
             :pagination="false"
             size="small"
+            :defaultExpandAllRows="true"
         >
           <template #name="{ text, record }">
             {{record.sort}} {{text}}
@@ -134,6 +136,7 @@ export default defineComponent({
     * }]
     * */
     const level1 = ref();
+    level1.value = [];
     /**
      * 数据查询
      **/
