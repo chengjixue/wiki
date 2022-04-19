574d74c8fd46747ba74b9de2f414c9f4e4a7d862
xuecj <xuecjpersonal@163.com>
2022-04-19 20:49:47 +0800

    9-9 增加redis测试接口

--- src/main/java/edu/xuecj/wiki/controller/TestController.java
+++ src/main/java/edu/xuecj/wiki/controller/TestController.java
@@ -3,7 +3,9 @@ package edu.xuecj.wiki.controller;
 import edu.xuecj.wiki.domain.Test;
 import edu.xuecj.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,14 +26,24 @@ public class TestController {
     private String testHello;
     @Resource
     private TestService testService;
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @RequestMapping("/hello")
     public String hello() {
         return "hello" + testHello;
     }
+
     @GetMapping("/test/list")
-    public List<Test> list(){
+    public List<Test> list() {
         return testService.list();
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object obj = redisTemplate.opsForValue().get(key);
+        System.out.println(key+ ":======================================" + obj);
+        return obj;
     }
 
 }
