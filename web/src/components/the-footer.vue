d767f24885f387e2f760d4d911239ec0c0db9a7d
xuecj <xuecjpersonal@163.com>
2022-04-19 21:16:59 +0800

    9-9 使用vuex保存用户信息，方便其他组件获取登录信息，遗留问题，刷新后值清空

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,12 +1,19 @@
 <template>
   <a-layout-footer style="text-align: center">
-    Java电子书
+    Java电子书  欢迎：{{user.name}}
   </a-layout-footer>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {computed, defineComponent} from 'vue';
+import store from "@/store";
 
 export default defineComponent({
   name: 'the-footer',
+  setup(){
+    const user=computed(()=> store.state.user);
+    return {
+      user
+    }
+  }
 });
 </script>
--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -49,6 +49,7 @@
 import {defineComponent, ref} from 'vue';
 import axios from "axios";
 import {message} from "ant-design-vue";
+import store from "@/store";
 
 declare let hexMd5: any;
 declare let KEY: any;
@@ -82,6 +83,7 @@ export default defineComponent({
           message.success('登录成功');
           user.value=data.content;
           console.log(user.value.name);
+          store.commit('setUser',user.value);
         }else {
           message.error(data.message);
         }
