5ccccf8084171a3240d17986fcb4eb15acce2bc3
xuecj <xuecjpersonal@163.com>
2022-04-25 21:36:12 +0800

    11-6 将欢迎文字提取成组件

---
+++ web/src/components/the-welcome.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Welcome to 知识库</h1>
+</template>
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'the-welcome',
+  setup() {
+    return {}
+  }
+});
+</script>
--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -25,7 +25,7 @@
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <div class="welcome" v-show="isShowWelcome">
-        <h1>欢迎使用java知识库</h1>
+        <the-welcome></the-welcome>
       </div>
       <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }"
               :data-source="ebooks">
@@ -65,6 +65,7 @@ import {defineComponent, onMounted, ref, reactive} from 'vue';
 import axios from 'axios';
 import {message} from 'ant-design-vue';
 import {Tool} from "@/util/tool";
+import TheWelcome from '@/components/the-welcome.vue';
 
 // const listData: any = [];
 //
@@ -83,6 +84,9 @@ import {Tool} from "@/util/tool";
 
 export default defineComponent({
   name: 'Home',
+    components: {
+      TheWelcome
+},
   setup() {
     const ebooks = ref();
     const level1 = ref();
